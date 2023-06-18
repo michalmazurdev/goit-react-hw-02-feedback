d1676cc7fd47617514d308e15b22c72636f8a9ca
michalmazurdev <m.mazur40@gmail.com>
2023-06-19 01:26:35 +0200

    styles added

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import css from './Statistics.module.css';
 export const Statistics = ({
   good,
   neutral,
@@ -9,11 +9,13 @@ export const Statistics = ({
   positivePercentage,
 }) => (
   <div>
-    <span>Good: {good}</span>
-    <span>Neutral: {neutral}</span>
-    <span>Bad: {bad}</span>
-    <span>Total: {total}</span>
-    <span>Positive feedback: {positivePercentage}%</span>
+    <span className={css.statisticsItem}>Good: {good}</span>
+    <span className={css.statisticsItem}>Neutral: {neutral}</span>
+    <span className={css.statisticsItem}>Bad: {bad}</span>
+    <span className={css.statisticsItem}>Total: {total}</span>
+    <span className={css.statisticsItem}>
+      Positive feedback: {positivePercentage}%
+    </span>
   </div>
 );
 Statistics.prpTypes = {
